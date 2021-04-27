f903041d4aa2a52b00c527596a9191a67313ab56
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-04-27 15:40:53 +0300

    TableViewController was added.

--- Storm Viewer/ViewController.swift
+++ Storm Viewer/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     var pictutes = [String]()
 
@@ -25,7 +25,16 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictutes[indexPath.row]
+        return cell
+    }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictutes.count
+    }
 
 }
 
