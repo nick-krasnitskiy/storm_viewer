77e3f1c9a5a222b763d7cc8e49b4d69feaf0550e
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-04-27 12:02:59 +0300

    Listing images with FileManager was added.

--- Storm Viewer/ViewController.swift
+++ Storm Viewer/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pictutes = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default //
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictutes.append(item)
+            }
+        }
+        
     }
 
 
