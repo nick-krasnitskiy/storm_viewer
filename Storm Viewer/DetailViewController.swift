3fd8af6343e148c2015122db257634fe2c0faf94
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-04-28 08:44:18 +0300

    Detail Screen was added.

---
+++ Storm Viewer/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  Storm Viewer
+//
+//  Created by Nick Krasnitskiy on 28.04.2021.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Storm Viewer/ViewController.swift
+++ Storm Viewer/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let fm = FileManager.default //
+        let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
         
